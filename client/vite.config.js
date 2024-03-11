6c99966f48b4d91a7139b6418175cc7263896388
Shambhabya Medhi <109235127+shambhabya@users.noreply.github.com>
2024-03-12 01:40:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8800',
+        target: 'https://aniflix-fest.onrender.com',
       },
     },
   },
